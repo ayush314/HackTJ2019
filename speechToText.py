c72514c30057a0bb5f055a420b28bf485a29165b
Ayush Varshney <34279863+blast314@users.noreply.github.com>
2019-04-07 04:49:33 -0400

    Update speechToText.py

--- speechToText.py
+++ speechToText.py
@@ -2,28 +2,24 @@ import os
 import speech_recognition as sr
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# file_dirname = dir_path + '\\uploads\\' + input()
-
-r = sr.Recognizer()
-mic = sr.Microphone()
+#r = sr.Recognizer()
+#mic = sr.Microphone()
 #egg, apple, banana, carrot, orange, bread
 #shirt, pants, sweater, socks, hat, shoes
 #blanket, tissues, comb, chapstick, bottle, backpack 
-thingsWeGive = ["blanket", "blankets", "tissues", "comb", "chapstick", "bottle", "bottles", "backpack", "backpacks", "shirt", "shirts", "pants","hat", "hats", "shoes", "socks", "sweater", "sweaters", "apple", "apples", "banana", "bananas", "carrot", "carrots", "egg", "eggs", "orange", "oranges", "bread"]
+#thingsWeGive = ["blanket", "blankets", "tissues", "comb", "chapstick", "bottle", "bottles", "backpack", "backpacks", "shirt", "shirts", "pants","hat", "hats", "shoes", "socks", "sweater", "sweaters", "apple", "apples", "banana", "bananas", "carrot", "carrots", "egg", "eggs", "orange", "oranges", "bread"]
+#numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "one", "two", "too", "to", "three", "tree", "four", "for", "five", "six", "seven", "eight", "nine", "ten"]
+#thingsTheyNeed = []
+ hellow = sr.AudioFile(dirpath + '\\uploads\\temp.wav')
+ with hellow as source:
+    audio = r.record(source)
+s = r.recognize_google(audio)
+#print("Text: " + s)
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "one", "two", "too", "to", "three", "tree", "four", "for", "five", "six", "seven", "eight", "nine", "ten"]
-thingsTheyNeed = []
-# hellow = sr.AudioFile(file_dirname + '.wav')
-# with hellow as source:
-#    audio = r.record(source)
-# try:
-#     s = r.recognize_google(audio)
-#     print("Text: " + s)
-# except Exception as e:
-#     print("Exception: " + str(e))
-# with mic as source:
-#     r.adjust_for_ambient_noise(source)
-#     audio = r.listen(source)
+"""
+with mic as source:
+     r.adjust_for_ambient_noise(source)
+     audio = r.listen(source)
 
 
 def listen(r,mic):
@@ -36,12 +32,14 @@ def listen(r,mic):
            return "I could not hear you!"
         except speech_recognition.UnknownValueError:
             return "I did not quite get that"
-        except speech_recognition.RequestError as e:
+        except speech_recognition.RequestErrro as e:
             return "Recognition Error: {0}".format(e) 
-print("starting")
+print("start")
 
-out = listen(r, mic).split()
-print(out)
+
+out = listen(r, mic)
+
+
 for i in range(len(out)):
 	if(out[i] in thingsWeGive):
 		if(out[i-1] in numbers):
@@ -50,3 +48,15 @@ for i in range(len(out)):
 
 
 print(thingsTheyNeed)
+"""
+#35.188.226.236
+#35.245.40.128
+_url = http://domain.com.domain.com.com/post_req
+_params = {
+id: [id]
+text: [s]
+addressed: false
+user: ''
+}
+
+res = requests.post(url = _url, params = _params)
